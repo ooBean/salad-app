74b16bc6b6f2aa0599d8be6073e7b0a81f4f4514
ooBean <littlebeancat@outlook.com>
2025-11-17 10:57:53 +0800

    fix: remove base config for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/salad-app/',
   plugins: [
     react(), 
     tsconfigPaths(),
